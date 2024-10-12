53831095056a73ad6d91c144c517958446a45969
sefesquivel <jesquivel@gmail.com>
2024-10-13 07:43:39 +0800

    Add files via upload

---
+++ backend/routes/student.route.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const studentRoute = express.Router();
+let StudentModel = require('../models/Student');
+
+// Create a new student
+studentRoute.route('/create-student').post(async (req, res, next) => {
+  try {
+    const data = await StudentModel.create(req.body);
+    res.json(data);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Get all students
+studentRoute.route('/').get(async (req, res, next) => {
+  try {
+    const data = await StudentModel.find();
+    res.json(data);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Get a specific student by ID
+studentRoute.route('/edit-student/:id').get(async (req, res, next) => {
+  try {
+    const data = await StudentModel.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Update a student
+studentRoute.route('/update-student/:id').put(async (req, res, next) => {
+  try {
+    const data = await StudentModel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    res.json(data);
+    console.log('Student successfully updated!');
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Delete a student
+studentRoute.route('/delete-student/:id').delete(async (req, res, next) => {
+  try {
+    const data = await StudentModel.findByIdAndRemove(req.params.id);
+    res.status(200).json({ msg: data });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = studentRoute;
